72a3c69b3a4f2c12efc31d63f467c1282ae7f1b0
Mustafa Zırhlı <Mustafazirhli98@gmail.com>
2024-07-18 18:50:55 +0300

    about me contact component created

--- app/aboutPage/page.tsx
+++ app/aboutPage/page.tsx
@@ -7,6 +7,7 @@ import COLOR_PALETTE from "../../lib/constants/colorPalette";
 import { CV_LIST_DATA } from "./../../lib/data/cvListData";
 import Breadcrumps from "../../components/ui/BreadCrumps";
 import ListOverlay from "../../components/cvList/ListOverlay";
+import ContactInfo from "./../../components/ContactInfo"
 import Footer from "./../../components/ui/Footer"
 import styles from "./page.module.css";
 
@@ -22,20 +23,7 @@ export default function AboutMe() {
                         <div className={styles.personalInfo}>
                             <div className={styles.summaryContainer}>
                                 <h5 className="font-semibold" style={{ color: COLOR_PALETTE.blue }}>Summary</h5>
-                                <ul className={styles.contactList}>
-                                    <li>
-                                        <FaGithub className="w-5 h-5 mr-2" />
-                                        <Link className="" href={"https://github.com/Mustafazirhli98"}>{infoPersonal.githubAdress}</Link>
-                                    </li>
-                                    <li>
-                                        <FaLinkedin className="w-5 h-5 mr-2" />
-                                        <Link href={"https://www.linkedin.com/in/mustafazirhli/"}>{infoPersonal.linkednAdress}</Link>
-                                    </li>
-                                    <li>
-                                        <IoMail className="w-5 h-5 mr-2" />
-                                        <Link href="mailto:mustafazirhli98@gmail.com">{infoPersonal.mailAdress}</Link>
-                                    </li>
-                                </ul>
+                                <ContactInfo visibility={"hidden lg:flex"}/>
                             </div>
                             <p className="text-sm lg:text-base mt-5" style={{ color: COLOR_PALETTE.dark }}>{infoPersonal.summaryText}</p>
                         </div>
--- components/ui/Footer.jsx
+++ components/ui/Footer.jsx
@@ -1,30 +1,14 @@
-import Link from "next/link";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { IoMail } from "react-icons/io5";
 import COLOR_PALETTE from "../../lib/constants/colorPalette";
-import { infoPersonal } from "../../lib/constants/InfoPersonal";
+import ContactInfo from "../ContactInfo";
 
 export default function Footer({ visibility }) {
     return (
         <div
-            className={`flex flex-col visible p-1 ${visibility}`}
+            className={`flex flex-col p-1 ${visibility}`}
             style={{ backgroundColor: COLOR_PALETTE.blue, color: COLOR_PALETTE.light }}
         >
             <h1 className="text-center border-b border-b-[#95a5a6] p-2">CONTACT</h1>
-            <ul className="p-2">
-                <li className="flex text-xs mt-2">
-                    <FaGithub className="w-5 h-5 mr-2" />
-                    <Link className="" href={"https://github.com/Mustafazirhli98"}>{infoPersonal.githubAdress}</Link>
-                </li>
-                <li className="flex mr-5 text-xs mt-2">
-                    <FaLinkedin className="w-5 h-5 mr-2" />
-                    <Link href={"https://www.linkedin.com/in/mustafazirhli/"}>{infoPersonal.linkednAdress}</Link>
-                </li>
-                <li className="flex text-xs mt-2">
-                    <IoMail className="w-5 h-5 mr-2" />
-                    <Link href="mailto:mustafazirhli98@gmail.com">{infoPersonal.mailAdress}</Link>
-                </li>
-            </ul>
+            <ContactInfo />
         </div>
     )
 }
